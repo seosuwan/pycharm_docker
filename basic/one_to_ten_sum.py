335d98d98a96ee7d4a1879e88b48bd4f20e5c652
seosuwan <los2ve7@gmail.com>
2021-10-07 11:36:34 +0900

    1007

--- basic/one_to_ten_sum.py
+++ basic/one_to_ten_sum.py
@@ -1,3 +1,7 @@
+'''
+파이썬에서 루프 형태는 3가지가 있다.
+'''
+from icecream import ic
 class OneToTenSum():
 
     def one_to_ten_sum_1(self):
@@ -5,11 +9,17 @@ class OneToTenSum():
         sum = 0
         for i in range(1, 11):
             sum += i
-        print(sum)
+        return sum
 
     def one_to_ten_sum_2(self):
-        print(sum(i for i in range(1, 11)))
+        return sum(i for i in range(1, 11)) #내장함수일때만 리스트컴프레션이 된다.
 
     def one_to_ten_sum_3(self):
-        print(sum(range(1,11)))
+        return sum(range(1,11))
+
+if __name__ == '__main__':
+    a = OneToTenSum()
+    ic(a.one_to_ten_sum_1())
+    ic(a.one_to_ten_sum_2())
+    ic(a.one_to_ten_sum_3())
 
